82a6cdf53410fae8a4c714118859fb9ff7f7ef51
M-Umair243 <133594111+M-Umair243@users.noreply.github.com>
2025-02-05 17:27:05 +0500

    Update page.jsx

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -2,6 +2,10 @@
 import React, { use, useEffect, useState } from 'react'
 import Image from 'next/image'
 import Loader from "../../public/loader.webp"
+export const metadata = {
+  title: 'Trailler Movies| Films',
+  description: 'Trailers of Movies in website',
+}
 const page = ({ params }) => {
   const { slug } = params
   const [movies, setMovies] = useState(null);
